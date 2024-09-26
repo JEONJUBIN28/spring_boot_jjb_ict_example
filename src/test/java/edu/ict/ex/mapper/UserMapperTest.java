12ca397481ee39217cbea5c982d94a4a31c8bd01
WIN-PKVVS2HE0GA\ICT02_02 <ehowlaksla@gmail.com>
2024-09-26 18:19:00 +0900

    암호화 완료

--- src/test/java/edu/ict/ex/mapper/UserMapperTest.java
+++ src/test/java/edu/ict/ex/mapper/UserMapperTest.java
@@ -1,10 +1,13 @@
 package edu.ict.ex.mapper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edu.ict.ex.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +19,7 @@ class UserMapperTest {
 	@Autowired
 	private UserMapper userMapper;
 	
+	@Disabled
 	@Test
 	void testUserMapper() {
 		assertNotNull(userMapper);
@@ -27,6 +31,41 @@ class UserMapperTest {
 		assertNotNull(user);
 		
 		System.out.println(user);
+		
+	} 
+	
+	//단방향(MD5, SHA등)은 암호화 (encoding)는 가능해도 복호화 (decoding)는 안되는것을 말하고,
+	//양방향 (AES, RSA, DES등)은 암호화 및 복호화가 가능한 것이다.
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Disabled
+	@Test
+	void testInsertUser() {
+		UserVO user = new UserVO();
+		user.setUsername("admin2");
+		user.setPassword(passwordEncoder.encode("admin2"));
+		user.setEnabled("1");
+		
+		userMapper.insertUser(user);
+		userMapper.insertAuthorities(user);
+		
+		assertNotNull(user);
+		
+		System.out.println(user);
+		
+	} 
+	
+	@Test
+	void testMatcher() {
+		
+		UserVO user = userMapper.getUser("admin2");
+		//$2a$10$CsuD0dUl/uvLumOujQlilu/xlAyuXcQoqHVqyLXshomeYfhXXQ7fq
+		boolean isMatch = passwordEncoder.matches("admin2", user.getPassword());
+		
+		System.out.println(user.getPassword());
+		assertEquals(isMatch, true);
 		
 	} 
 	
